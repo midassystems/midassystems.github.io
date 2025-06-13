ede543cff7edc88467db875643a2ebac9a95d41b
anthonyb8 <anthonybaxter819@gmail.com>
2025-06-13 16:48:24 -0400

    Initial website.

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,17 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>Algorithimica tradon made simple.</h1>
+        <h1>Algorithmic trading made simple.</h1>
         <h3>
           Whether you are a seasoned professional or a beginner the Midas System
-          provide s eveyrhtign uou nee to build & strategies
+          providess everything you need to build & deploy strategies.
         </h3>
         <Socialbar />
-        <a href="https://midastrader.readthedocs.io/en/latest/">
-          <button> Get Started</button>
-        </a>
+        <div className={styles.button_container}>
+          <a href="https://midastrader.readthedocs.io/en/latest/">
+            <button> Get Started</button>
+          </a>
+        </div>
       </main>
     </div>
   );
--- app/ui/top-nav.tsx
+++ app/ui/top-nav.tsx
@@ -1,7 +1,4 @@
 import styles from "@/app/ui/top-nav.module.css";
-import { FaGithub } from "react-icons/fa6";
-import { FaLinkedin } from "react-icons/fa";
-import { FaMedium } from "react-icons/fa6";
 
 export default function TopNav() {
   return (
@@ -11,17 +8,11 @@ export default function TopNav() {
         <h1>idas</h1>
       </div>
       <nav className={styles.nav}>
-        <a href="#about" className={styles.top_nav_icons}>
-          About
-        </a>
-        <a href="#experience" className={styles.top_nav_icons}>
-          Experience
-        </a>
-        <a href="#projects" className={styles.top_nav_icons}>
-          Projects
-        </a>
-        <a href="#contact" className={styles.top_nav_icons}>
-          Contact
+        <a
+          href="https://midastrader.readthedocs.io/en/latest/"
+          className={styles.top_nav_icons}
+        >
+          Documentation
         </a>
       </nav>
     </div>
